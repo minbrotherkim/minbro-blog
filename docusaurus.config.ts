112271a88038403761c209c08ce7dd0cca9fc853
Minbro <minbrotherkim@gmail.com>
2024-12-17 03:19:44 +0900

    vercel.json, config 수정

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -2,13 +2,15 @@ import {themes as prismThemes} from 'prism-react-renderer';
 import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
+const isVercel = process.env.VERCEL === '1';
+
 const config: Config = {
   title: 'Minbro\'s Blog',
   tagline: '여행 기록, 그리고 회고',
   favicon: 'img/nuo_favicon.ico',
 
   url: 'https://minbrotherkim.github.io',
-  baseUrl: '/minbro-blog/',
+  baseUrl: isVercel ? '/' : '/minbro-blog/',
 
   organizationName: 'minbrotherkim',
   projectName: 'minbro-blog',
