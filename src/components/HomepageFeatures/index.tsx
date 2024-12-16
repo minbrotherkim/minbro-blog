27c1cadcc063c411e038fde48e3e09ede24b284a
minbrotherkim <minbrotherkim@nate.com>
2024-12-16 16:14:56 +0900

    홈 화면 본문 링크 추가

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -29,7 +29,7 @@ const FeatureList: FeatureItem[] = [
             <>
                 사촌동생과 <Link to="/docs/해외여행/일본/나고야">나고야</Link> 근교 나가시마 스파랜드에서 롤러코스터를 마음껏
                 타고, <Link to="/docs/해외여행/일본/다카야마,%20시라카와고">다카야마와 시라카와고</Link> 버스투어를 하며 소도시를
-                구경했던 4박5일간의 여행
+                구경했던 4박5일간의 <Link to="/docs/해외여행/일본">일본</Link> 여행
             </>
         ),
     },
@@ -40,7 +40,7 @@ const FeatureList: FeatureItem[] = [
             <>
                 엄마와 함께한 <Link to="/docs/해외여행/일본/다카마쓰">다카마쓰</Link>-<Link to="/docs/해외여행/일본/오카야마">오카야마</Link>-<Link to="/docs/해외여행/일본/히로시마">히로시마</Link> 여행
                 <br/>
-                다양한 전시와 박물관, 정원, 관광명소, 식당과 음식이 함께했던 즐거운 여행
+                다양한 전시와 박물관, 정원, 관광명소, 식당과 음식이 함께했던 즐거운 <Link to="/docs/해외여행/일본">일본</Link> 여행
             </>
         ),
     },
@@ -49,7 +49,7 @@ const FeatureList: FeatureItem[] = [
         ImgSrc: require('@site/static/img/ayutaya.jpeg').default,
         description: (
             <>
-                나의 첫 태국 여행이자, 2024년 첫 가족 여행
+                나의 첫 <Link to="/docs/해외여행/태국">태국</Link> 여행이자, 2024년 첫 가족 여행
                 <br/>
                 왕궁과 음식이 기억에 남는 <Link to="/docs/해외여행/태국/방콕">방콕</Link>, 그리고 옛 터가 생각보다 웅장했던 <Link to="/docs/해외여행/태국/아유타야">아유타야</Link>
             </>
@@ -60,7 +60,7 @@ const FeatureList: FeatureItem[] = [
         ImgSrc: require('@site/static/img/tokyo_cousin.jpeg').default,
         description: (
             <>
-                10살 어린 사촌동생과 함께 여행은 처음!
+                10살 어린 사촌동생과 함께 <Link to="/docs/해외여행/일본">일본</Link> 여행은 처음!
                 <br/>
                 갈 때 마다 항상 먹을 것도, 볼 것도 많아서 즐거운 <Link to="/docs/해외여행/일본/도쿄">도쿄</Link>
             </>
@@ -72,7 +72,6 @@ const FeatureList: FeatureItem[] = [
         description: (
             <>
                 내가 거의 모든 일정을 짠 건 처음이었던 가족 여행
-                <br/>
                 온천과 정말 맛있는 일식 부페를 먹었던 <Link to="/docs/해외여행/일본/노보리베츠">노보리베츠</Link>
                 <br/>
                 파릇파릇한 나무들 사이를 열심히 걸었던 <Link to="/docs/해외여행/일본/삿포로">삿포로</Link>
@@ -86,7 +85,7 @@ const FeatureList: FeatureItem[] = [
         ImgSrc: require('@site/static/img/kitakyushu.jpeg').default,
         description: (
             <>
-                예전 회사 팀원분과 함께는 처음이었던 일본 여행
+                예전 회사 팀원분과 함께는 처음이었던 <Link to="/docs/해외여행/일본">일본</Link> 여행
                 <br/>
                 너무 맛있어서 이틀 동안 두번 간 초밥 시장이 있던 <Link to="/docs/해외여행/일본/기타큐슈">기타큐슈</Link>, 
                 잘 먹고 심지어 헬스장에서 운동까지 했던 <Link to="/docs/해외여행/일본/후쿠오카">후쿠오카</Link>
